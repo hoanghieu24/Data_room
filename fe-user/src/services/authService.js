7eb1443ef385a7b8889fb8ef71c0a96db3fd05b3
hoanghieu24 <162785962+hoanghieu24@users.noreply.github.com>
2026-04-20 17:02:41 +0700

    Update authService.js

--- fe-user/src/services/authService.js
+++ fe-user/src/services/authService.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 
+const API_BASE_URL =
+  import.meta.env.VITE_API_URL_DEPLOY || "http://localhost:3000/api";
+
 export const login = async (data) => {
-  const res = await axios.post("/api/auth/login", data);
+  const res = await axios.post(`${API_BASE_URL}/auth/login`, data);
   return res.data;
 };
