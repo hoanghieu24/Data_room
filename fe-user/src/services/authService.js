cba7496ba33754e3fb5304db32ae261b0a0fa2a7
hoanghieu24 <162785962+hoanghieu24@users.noreply.github.com>
2026-04-21 11:01:39 +0700

    Update authService.js

--- fe-user/src/services/authService.js
+++ fe-user/src/services/authService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API_BASE_URL =
-  import.meta.env.VITE_API_URL_DEPLOY || "http://localhost:3000/api";
+  import.meta.env.VITE_API_URL_DEPLOY || "https://dataroom-production-2571.up.railway.app/api";
 
 export const login = async (data) => {
   const res = await axios.post(`${API_BASE_URL}/auth/login`, data);
