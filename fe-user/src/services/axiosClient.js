2185080026ed6c888f83a828acfe627d0b1dd17e
hoanghieu24 <162785962+hoanghieu24@users.noreply.github.com>
2026-04-21 16:31:25 +0700

    Update axiosClient.js

--- fe-user/src/services/axiosClient.js
+++ fe-user/src/services/axiosClient.js
@@ -1,18 +1,1 @@
-import axios from "axios";
-
-const axiosClient = axios.create({
-  baseURL: import.meta.env.VITE_API_URL_DEPLOY,
-  headers: {
-    "Content-Type": "application/json",
-  },
-});
-
-axiosClient.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
-});
-
-export default axiosClient;
+import axios from "axios"; const axiosClient = axios.create({ baseURL: import.meta.env.VITE_API_URL_DEPLOY, }); axiosClient.interceptors.request.use((config) => { const token = localStorage.getItem("token"); if (token) { config.headers.Authorization = Bearer ${token}; } return config; });
