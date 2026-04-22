7c74cf5bb60386c0c320044a6bb63ff16cd6ab2d
hoanghieu24 <162785962+hoanghieu24@users.noreply.github.com>
2026-04-22 09:42:24 +0700

    Update userService.js

--- fe-user/src/services/userService.js
+++ fe-user/src/services/userService.js
@@ -25,7 +25,7 @@ export const createUser = async (userData) => {
 // Cập nhật user
 export const updateUser = async (userId, userData) => {
   try {
-    const res = await axiosClient.put(`/${userId}`, userData);
+    const res = await axiosClient.put(`/admin/${userId}`, userData);
     return res.data;
   } catch (err) {
     console.error("updateUser error:", err);
@@ -36,7 +36,7 @@ export const updateUser = async (userId, userData) => {
 // Xóa user
 export const deleteUser = async (userId) => {
   try {
-    const res = await axiosClient.delete(`/${userId}`);
+    const res = await axiosClient.delete(`/admin/${userId}`);
     return res.data;
   } catch (err) {
     console.error("deleteUser error:", err);
